28f3f7d38af6be154e3f94ac4138c1bc49e7f1af
Smita.Dasgupta <smitadasgupta05@gmail.com>
2018-01-11 20:00:25 +0000

    Formatting amount and create time

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -14,7 +14,7 @@ class ExpenseForm extends React.Component {
         this.state = {
             description: props.expense ? props.expense.description : '',
             note: props.expense ? props.expense.note : '',
-            amount: props.expense ? (props.expense.amount).toString() : '',
+            amount: props.expense ? (props.expense.amount/100).toString() : '',
             createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calendarFocused: false,
             error: ''
